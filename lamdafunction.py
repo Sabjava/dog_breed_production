4650f4f2fc5913f4e60f9eea600ed069a614aebe
Svetlana Bogdanova <sab@Svetlanas-MacBook-Air.local>
2024-12-09 17:49:32 -0800

    lambda and notebook

--- lamdafunction.py
+++ lamdafunction.py
@@ -10,7 +10,7 @@ logger.setLevel(logging.DEBUG)
 print('Loading Lambda function')
 
 runtime=boto3.Session().client('sagemaker-runtime')
-endpoint_Name='BradTestEndpoint'
+endpoint_Name='pytorch-inference-2024-12-09-02-08-34-307'
 
 def lambda_handler(event, context):
 
@@ -40,3 +40,4 @@ def lambda_handler(event, context):
         #'updated_result':str(updated_result)
 
         }
+    
